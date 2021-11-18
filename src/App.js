6fa1705bc6906a1d0e2b7947c75c603f00640b27
Richard <33560310+richardchyan@users.noreply.github.com>
2021-11-17 22:28:25 -0500

    initial nav and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Blog from './containers/Blog';
+import Brand from './containers/Brand';
+import Footer from './containers/Footer';
+import Header from './containers/Header';
+import WhatGPT3 from './containers/WhatGPT3';
+import Possibility from './containers/Possibility';
+import CallToAction from './components/CallToAction';
+import Features from './components/Features';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App  w-full h-screen">
+      <div className="">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CallToAction />
+      <Blog />
+      <Footer />
     </div>
   );
 }
