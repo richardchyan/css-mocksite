9df90f892198c6a2bf37118cd55c83c95cd88176
Richard <33560310+richardchyan@users.noreply.github.com>
2021-11-21 22:43:28 -0500

    responsive fixes to mobile L to tablet and above

--- src/containers/Possibility.js
+++ src/containers/Possibility.js
@@ -4,7 +4,7 @@ import headset from '../assets/possibility.png';
 const Possibility = () => {
    return (
       <div id="possibility" className="mt-10">
-         <div className="flex max-w-screen-lg m-auto justify-between space-x-10 px-10">
+         <div className="flex flex-col lg:flex-row max-w-screen-lg m-auto  px-10">
             {/* image */}
             <div>
                <img src={headset} alt="VR headset" />
@@ -13,7 +13,7 @@ const Possibility = () => {
             <div className="flex flex-col space-y-9 justify-center">
                <p className="text-lg text-blue-300 font-medium text-left">Request Early Access to Get Started</p>
                <h1 className="text-4xl bg-gradient-to-r from-red-400 via-blue-400 to-blue-200 text-transparent
-               bg-clip-text text-left">The possibilities are beyond your imagination</h1>
+               bg-clip-text text-left pb-1">The possibilities are beyond your imagination</h1>
                <h2 className="text-sm text-left font-thin text-blue-200">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</h2>
                <p className="text-red-400 text-left">Request Early Access to Get Started</p>
             </div>
--- src/containers/WhatGPT3.js
+++ src/containers/WhatGPT3.js
@@ -6,20 +6,20 @@ const WhatGPT3 = () => {
          {/* main container */}
          <div className="text-blue-50 w-3/4 max-w-screen-lg p-8 m-auto bg-gradient-to-br from-purple-900 via-indigo-800 to-blue-900 space-y-6">
             {/* info container */}
-            <div className="flex justify-between">
-               <h1 className="text-lg text-left w-1/4">What is GPT-3?</h1>
-               <p className="text-sm text-left font-thin text-blue-200 w-2/3">We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+            <div className="flex flex-col lg:flex-row justify-between">
+               <h1 className="text-lg text-left lg:w-1/4">What is GPT-3?</h1>
+               <p className="text-sm text-left font-thin text-blue-200 lg:w-2/3">We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
             </div>
             {/* heading container */}
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center">
                <h2 className="text-2xl bg-gradient-to-r from-red-400 via-blue-400 to-blue-200 text-transparent
-               bg-clip-text text-left w-2/4 border-blue-300">
+               bg-clip-text text-left lg:w-2/4 border-blue-300">
                   The possibilities are beyond your imagination
                </h2>
-               <p className="text-md w-1/4">Explore the library</p>
+               <p className="text-md text-left lg:w-1/4">Explore the library</p>
             </div>
             {/* descriptions container */}
-            <div className="flex space-x-4">
+            <div className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-4">
                <div>
                   <h3 className="text-left" >Chatbots</h3>
                   <p className="text-sm text-left font-thin text-blue-200">We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
